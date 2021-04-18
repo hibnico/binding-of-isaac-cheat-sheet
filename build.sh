448c03c452eab7c70c7f7d17ebbab07907a0b6cf
Nicolas Lalevée <nicolas.lalevee@hibnet.org>
2021-04-18 16:24:55 +0200

    Add stones and others

--- build.sh
+++ build.sh
@@ -7,6 +7,7 @@ pages=(\
   "Passive_Collectibles" \
   "All_Trinkets" \
   "Major_Arcana_Cards" \
+  "Reversed_Major_Arcana_Cards" \
   "Playing_Cards" \
   "Credit_Card_Back" \
   "Other_Playing_Card_Backs" \
@@ -17,6 +18,8 @@ pages=(\
   "Left_Pointing_Runes" \
   "Right_Pointing_Runes" \
   "Other_Runes" \
+  "Soul_Stones" \
+  "Other"
 )
 
 cp header.html index.html
@@ -33,12 +36,12 @@ for p in "${pages[@]}"; do
     echo "<div class=\"category\" id=\"$p\">"
     echo "<h2>$title</h2>"
     echo "<div class=\"images\">"
-    cat "work/${p}_images.html"
+    cat "work/html/${p}_images.html"
     echo "<div class=\"isaac-descriptions\">"
-    cat "work/${p}_descriptions.html"
+    cat "work/html/${p}_descriptions.html"
     echo "</div></div></div>"
     echo "<script>all_names[\"${p}\"]="
-    cat "work/${p}_names.json"
+    cat "work/json/${p}_names.json"
     echo "</script>"
   } >> index.html
 done
--- extract.sh
+++ extract.sh
@@ -2,57 +2,61 @@
 
 set -euxo pipefail
 
-rm -rf work
-mkdir -p work
+rm -rf work/html
+rm -rf work/json
+mkdir -p work/html
+mkdir -p work/json
 
 extract_table () {
-  cat "$1" | sed -n "/id=\"$2\"/,/<\/table>/{p}" | sed -n '/<table/,/<\/table>/{p}' > "work/$2.html"
+  cat "$1" | sed -n "/id=\"$2\"/,/<\/table>/{p}" | sed -n '/<table/,/<\/table>/{p}' > "work/html/$2.html"
 }
 
 extract_images () {
-  xpath -q -e '/table/tbody/tr/td[3]' "work/$1.html" \
+  xpath -q -e '/table/tbody/tr/td[3]' "work/html/$1.html" \
     | sed -e 's/<img/<img referrerpolicy="no-referrer"/g' \
           -e 's/<div>/<span>/g' \
           -e 's/<\/div>/<\/span>/g' \
           -e "s/<td>/<div class=\"isaac-image\" data-isaac-category=\"$1\">/" \
           -e 's/<\/td>/<\/div>/' \
           -e 's/href="/target="_blank" href="https:\/\/bindingofisaacrebirth.fandom.com/' \
-    > "work/$1_images.html"
+    > "work/html/$1_images.html"
 }
 
 extract_description() {
-  echo "[" > "work/$1_names.json"
-  xpath -q -e '/table/tbody/tr/td[1]/@data-sort-value' "work/$1.html" | sed -e 's/.*\(".*"\)/\1,/' >> "work/$1_names.json"
-  echo "\"\"]" >> "work/$1_names.json"
-  xpath -q -e '/table/tbody/tr/td[5]' "work/$1.html" \
+  echo "[" > "work/json/$1_names.json"
+  xpath -q -e '/table/tbody/tr/td[1]/@data-sort-value' "work/html/$1.html" | sed -e 's/.*\(".*"\)/\1,/' >> "work/json/$1_names.json"
+  echo "\"\"]" >> "work/json/$1_names.json"
+  xpath -q -e "/table/tbody/tr/td[${2}]" "work/html/$1.html" \
     | sed -e 's/<td>/<div class="isaac-description">/' \
           -e 's/<\/td>/<\/div>/' \
           -e 's/<img/<img referrerpolicy="no-referrer"/g' \
           -e 's/href="/target="_blank" href="https:\/\/bindingofisaacrebirth.fandom.com/g' \
-    > "work/$1_descriptions.html"
+    > "work/html/$1_descriptions.html"
 }
 
-extract () {
+extract() {
   extract_table "$1" "$2"
   extract_images "$2"
-  extract_description "$2"
+  extract_description "$2" "$3"
 }
 
-extract raw/items.html "Activated_Collectibles"
-extract raw/items.html "Passive_Collectibles"
+extract raw/items.html "Activated_Collectibles" "5"
+extract raw/items.html "Passive_Collectibles" "5"
 
-extract raw/cards.html "Major_Arcana_Cards"
-extract raw/cards.html "Reversed_Major_Arcana_Cards"
-extract raw/cards.html "Playing_Cards"
-#extract raw/cards.html "#Magic:_The_Gathering_Card_Backs"
-extract raw/cards.html "Credit_Card_Back"
-extract raw/cards.html "Other_Playing_Card_Backs"
-extract raw/cards.html "Card_Against_Humanity_Back"
-extract raw/cards.html "Chance_Card_Back"
-extract raw/cards.html "Holy_Card_Back"
-extract raw/cards.html "Uno_Card_Back"
-extract raw/cards.html "Left_Pointing_Runes"
-extract raw/cards.html "Right_Pointing_Runes"
-extract raw/cards.html "Other_Runes"
+extract raw/cards.html "Major_Arcana_Cards" "5"
+extract raw/cards.html "Reversed_Major_Arcana_Cards" "6"
+extract raw/cards.html "Playing_Cards" "5"
+#extract raw/cards.html "#Magic:_The_Gathering_Card_Backs" "5"
+extract raw/cards.html "Credit_Card_Back" "5"
+extract raw/cards.html "Other_Playing_Card_Backs" "5"
+extract raw/cards.html "Card_Against_Humanity_Back" "5"
+extract raw/cards.html "Chance_Card_Back" "5"
+extract raw/cards.html "Holy_Card_Back" "5"
+extract raw/cards.html "Uno_Card_Back" "5"
+extract raw/cards.html "Left_Pointing_Runes" "5"
+extract raw/cards.html "Right_Pointing_Runes" "5"
+extract raw/cards.html "Other_Runes" "5"
+extract raw/cards.html "Soul_Stones" "5"
+extract raw/cards.html "Other" "5"
 
-extract raw/trinkets.html "All_Trinkets"
+extract raw/trinkets.html "All_Trinkets" "5"
